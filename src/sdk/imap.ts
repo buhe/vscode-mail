871bc8d55230d821393988e17021b76470aeb39d
buhe <bugu1986@126.com>
2022-07-30 15:14:53 +0800

    git-u:update

--- src/sdk/imap.ts
+++ src/sdk/imap.ts
@@ -29,30 +29,35 @@ export default class ImapFace {
         imap.once('ready', function () {
             openInbox(function (err: any, box: any) {
                 if (err) throw err;
-                var f = imap.seq.fetch('1:3', {
-                    bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-                    struct: true
-                });
-                f.on('message', function (msg: any, seqno: any) {
+                var f = imap.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM SUBJECT DATE)', 'TEXT'] });
+                f.on('message', function (msg, seqno) {
                     console.log('Message #%d', seqno);
                     var prefix = '(#' + seqno + ') ';
-                    msg.on('body', function (stream: any, info: any) {
-                        var buffer = '';
-                        stream.on('data', function (chunk: any) {
+                    msg.on('body', function (stream, info) {
+                        if (info.which === 'TEXT')
+                            console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
+                        var buffer = '', count = 0;
+                        stream.on('data', function (chunk) {
+                            count += chunk.length;
                             buffer += chunk.toString('utf8');
+                            if (info.which === 'TEXT')
+                                console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
                         });
                         stream.once('end', function () {
-                            console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+                            if (info.which !== 'TEXT')
+                                console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+                            else
+                                console.log(prefix + 'Body [%s] Finished', inspect(info.which));
                         });
                     });
-                    msg.once('attributes', function (attrs: any) {
+                    msg.once('attributes', function (attrs) {
                         console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
                     });
                     msg.once('end', function () {
                         console.log(prefix + 'Finished');
                     });
                 });
-                f.once('error', function (err: any) {
+                f.once('error', function (err) {
                     console.log('Fetch error: ' + err);
                 });
                 f.once('end', function () {
