1bfc06b5cf8746880fbb8343a1e9e041b5fb2fb0
buhe <bugu1986@126.com>
2022-07-31 21:46:47 +0800

    git-u:update

--- tests/index.ts
+++ tests/index.ts
@@ -2,26 +2,31 @@ import * as chai from 'chai';
 import Imap from '../src/sdk/imap';
 
 import * as RawImap from 'node-imap';
+import { assert } from 'chai';
 const inspect = require('util').inspect;
 function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 describe('imap', () => {
-    it('getBoxes',async () => {
-        let imap = Imap.connect();
+    let imap: any;
+    before(async function () {
+        imap = Imap.connect();
         await delay(5 * 1000);
+    });
+    after(function () {
+        imap.end();
+    });
+
+    it('getBoxes',async () => {
         let b = await imap.getBoxesAsync();
         console.log(b);
-        imap.end();
+        assert(Object.keys(b.length > 0));
     })
 
-    it('read selected mailbox',async () => {
-        let imap = Imap.connect();
-        await delay(5 * 1000);
+    it('read selected mailbox', async () => {
         let mails = await Imap.openMail('INBOX');
         console.log(mails);
-        await delay(5 * 1000);
-        imap.end();
+        assert(mails.length > 0);
     })
 })
