70ab53ad0cd5629a7ef4a64343cae9f42e3c7308
buhe <bugu1986@126.com>
2022-08-05 21:24:11 +0800

    git-u:update

--- src/tests/imap.ts
+++ src/tests/imap.ts
@@ -2,7 +2,10 @@ import * as chai from 'chai';
 import ImapFace from '../sdk/imap';
 import { assert } from 'chai';
 import { DISPLAY_KEY, IMAP_PORT_KEY, IMAP_SERVER_KEY, PASS_KEY, USER_KEY } from '../strategy';
-
+let { web: { client_id, client_secret, refresh_token } } = require('./client_secret') // the client_secret.json file
+var xoauth2 = require("xoauth2"),
+    xoauth2gen;
+var Imap = require('node-imap');
 // describe('imap-126', () => {
 //     let imapFace: ImapFace;
 //     let imap: any;
@@ -38,28 +41,52 @@ describe('imap-gmail', () => {
     let imapFace: ImapFace;
     let imap: any;
     before(async function () {
-        imapFace = new ImapFace({
-            [IMAP_SERVER_KEY]: 'gmail.126.com',
-            [IMAP_PORT_KEY]: 993,
-            [USER_KEY]: 'bugu1986@gmail.com',
-            [PASS_KEY]: '&pAdkpYt7GNLJ1@Z',
-            [DISPLAY_KEY]: 'mygmail',
-        });
-        imap = await imapFace.connect();
+        // imapFace = new ImapFace({
+        //     [IMAP_SERVER_KEY]: 'gmail.126.com',
+        //     [IMAP_PORT_KEY]: 993,
+        //     [USER_KEY]: 'bugu1986@gmail.com',
+        //     [PASS_KEY]: '&pAdkpYt7GNLJ1@Z',
+        //     [DISPLAY_KEY]: 'mygmail',
+        // });
+        // imap = await imapFace.connect();
     });
     after(function () {
-        imap.end();
+        // imap.end();
     });
 
     it('getBoxes', async () => {
-        let b = await imap.getBoxesAsync();
-        // console.log(b);
-        assert(Object.keys(b.length > 0));
+        // let b = await imap.getBoxesAsync();
+        // // console.log(b);
+        // assert(Object.keys(b.length > 0));
+        
+        let xoauth2gen = xoauth2.createXOAuth2Generator({
+            user: 'bugu1986@gmail.com', // the email address
+            clientId: client_id,
+            clientSecret: client_secret,
+            refreshToken: refresh_token
+        })
+
+        xoauth2gen.getToken(function (err: any, xoauth2token: any) {
+            if (err) {
+                return console.log(err)
+            }
+
+            let imap = new Imap({
+                xoauth2: xoauth2token,
+                host: 'imap.gmail.com',
+                port: 993,
+                tls: true,
+                authTimeout: 10000,
+                debug: console.log,
+            })
+
+            imap.connect();
+        });
     })
 
     it('read selected mailbox', async () => {
-        let mails = await imapFace.openMail('INBOX');
-        // console.log(mails);
-        assert(mails.length > 0);
+        // let mails = await imapFace.openMail('INBOX');
+        // // console.log(mails);
+        // assert(mails.length > 0);
     })
 })
